0fed93b60856b5bad693748db3f275f3a569b177
Kirill <lika_208@mail.ru>
2023-04-17 16:55:13 +0300

    2

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,18 +1,42 @@
-import { HttpException, Injectable } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
+import { HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { SAULT, UserService } from 'src/user/user.service';
 import { SignInDto } from './dto/signIn.dto';
+import * as bcrypt from 'bcrypt';
+import { TokenServiceService } from 'src/token-service/token-service.service';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private userService: UserService
+        private userService: UserService,
+        private tokenService: TokenServiceService
     ){}
 
     async login(signInDto: SignInDto){
         // Проверить аккаунт на статус активации
         // Найти пользователя 
         const {email, password} = signInDto
-        this.userService.getUserBy('email', '')
+        const user = await this.userService.getUserBy('email', email)
+        if ( !user ) throw new UnauthorizedException()
+        if ( !bcrypt.compareSync(password, user.password) )  throw new UnauthorizedException()
+        if ( !user.activationStatus ) throw new UnauthorizedException()
+
+        
+       
+        // console.log(userData)
+
+        // this.tokenService.generateAccessRefresh()
+        // Пользователь найден
+        // 
+      
+        return {
+            accessToken: '',
+            refreshToken: '',
+            userData: {
+                
+            }
+        }
+        
+ 
     }
 
     async registration(email: string, password: string){
