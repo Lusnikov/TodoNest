0f1463021c4d8e742261949b644dd638f9931eac
Kirill <lika_208@mail.ru>
2023-04-19 19:19:51 +0300

    Added edit form

---
+++ frontend/src/Components/editTodo/editTodoForm.tsx
@@ -0,0 +1,169 @@
+import React, { useEffect, useMemo, useRef, useState } from 'react'
+import Select from 'react-select'
+import TimePicker from 'react-time-picker';
+
+import { addTodo } from '@/store/slice/user.slice'
+import { useAppDispatch, useAppSelector } from '@/store/storeHooks'
+import { Section, Todos } from '@/types/types'
+import { Alert, Button, CloseButton, FormControl, FormErrorMessage, FormLabel, Heading, Input, Textarea, } from '@chakra-ui/react'
+import { useDefaultInput } from '@/hooks/useDefaultInput'
+
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-time-picker/dist/TimePicker.css';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import TodoDatePicker from '../todoForm/TodoDatePicker';
+
+type Props = {
+  todo: Todos,
+  // initialData: {
+  //   title: string,
+  //   content: string,
+  //   section: Section| null ,
+  //   date: Date
+  // },
+  sections: Section[],
+  onCloseButtonClick: () => void,
+  onSubmitForm: (data: Todos) => void,
+  message?: string | null ,
+  onChangeValid: (e: Todos| null) => void
+}
+
+
+type FormValues = Pick<Todos, 'title'|'content'|'dateEnded'|'section'>
+
+const EditFormTodo = (props: Props) => {
+  const {
+    todo, 
+    sections,
+    onSubmitForm, 
+    message=null,
+     onCloseButtonClick, 
+     onChangeValid
+  } = props
+  const { register, handleSubmit, formState, watch , formState: { isValidating }, setValue, getValues} = useForm<Todos>({
+    defaultValues:{
+        completed: todo.completed,
+        content:  todo.content,
+        dateCreated: todo.dateCreated,
+        dateEnded:  todo.dateEnded,
+        id: todo.id,
+        section: todo.section,
+        title: todo.title,
+    },
+  })
+
+  const dateEnded = watch('dateEnded') ?? null
+  const onSubmit:SubmitHandler<Todos> = (data) => onSubmitForm(data);
+
+  const data = watch();
+
+ 
+
+  React.useEffect(() => {
+    const isValid = formState.isValid && !isValidating;
+    onChangeValid(isValid ? data : null)
+    console.log(1)
+  }, [formState.isValid, JSON.stringify(data), isValidating]);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {todo.id}
+  <FormControl pt={10} isInvalid={true} >
+    <Heading mb={4}>
+      Редактирование Todo-элемента 
+    </Heading>
+    {message &&  <Alert >
+      {message}
+    </Alert> }
+  
+    <FormLabel>
+      Введите название
+    </FormLabel>
+    <Input 
+      required
+      placeholder='Заголовок Todo' 
+      maxW={'480px'}
+      {...register('title', {required: true})}
+
+    />
+    <FormErrorMessage>
+      {/* {todoError} */}
+    </FormErrorMessage>
+
+    <FormLabel>
+      Введите Контент
+    </FormLabel>
+
+    <Textarea 
+      placeholder="Введите текстовое содержание" 
+      {...register('content', {required: true})}
+    />
+    <FormErrorMessage>
+      {/* {contentError} */}
+    </FormErrorMessage>
+      
+    <FormLabel>
+      Время завершения туду (необязательно)
+    </FormLabel>
+
+    <TodoDatePicker
+      initialValue={dateEnded}
+      onChange={(date) => setValue('dateEnded',  date ?? undefined)}
+    />    
+
+    {/* {date &&
+      <TimePicker
+        value={time ?? "23:59"}
+        disableClock
+        onChange={e => setTime(e)}
+      />
+    } */}
+
+    <FormLabel>
+      Выберите группу (необязательно)
+    </FormLabel>
+
+    <Select 
+      isClearable
+      getOptionLabel={opt => opt.name}
+      getOptionValue={opt => opt.name}
+      placeholder={'Выберите группу'}
+   
+      // onChange={e =>  setGroup(e as Section)}
+      options={sections}
+      onChange={(option, triggeredAction) => {
+        // if (triggeredAction.action === 'clear') {
+        //   return setValue('section', undefined)
+        // }
+        return setValue('section', option ?? undefined, {shouldValidate: true})
+      }
+      }
+      
+    />
+
+    <Button 
+        // isLoading={isCreated} 
+        // isDisabled={submitDisabled} 
+        // onClick={createTodo}
+        mt={5}
+        type='submit'
+    >
+      Изменить
+    </Button>
+
+    <CloseButton
+      size="sm"
+      position="absolute"
+      right={2}
+      top={10}
+      onClick={() => {
+        onCloseButtonClick()
+      }}
+    />
+</FormControl>
+    </form>
+   
+  )
+}
+
+export default EditFormTodo
