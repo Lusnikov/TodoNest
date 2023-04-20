239469c7f1d52485c8b91b74b3145a0d2fce819a
Kirill <lika_208@mail.ru>
2023-04-20 21:06:18 +0300

    Added refresh endpooint

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, Res, UnauthorizedException } from '@nestjs/common';
 import { RegistrationDto } from './dto/registration.dto';
 import { SignInDto } from './dto/signIn.dto';
 import { AuthService } from './auth.service';
@@ -6,12 +6,18 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserActivation } from 'src/Entities/UserActivation.entity';
 import { Repository } from 'typeorm';
 import { SignInResponse } from './types/types';
-import { Response } from 'express';
+import { Request, Response } from 'express';
+import { TokenServiceService } from 'src/token-service/token-service.service';
+import { ClientUserData } from 'types/types';
+import { UserService } from 'src/user/user.service';
+import { UserDto } from 'src/dto/User.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor (
         private  authService: AuthService,
+        private userService: UserService,
+        private readonly tokenService: TokenServiceService
       
 
     ){
@@ -36,8 +42,26 @@ export class AuthController {
         }
     }
     @Post('refresh')
-    async refresh(){
-        return 'refresh'
+    async refresh(@Req() req: Request, @Res({passthrough: true}) res: Response): SignInResponse{
+        const refreshToken: string = req.cookies.refresh  ?? null
+        if (!refreshToken) throw new UnauthorizedException()
+        const userData = this.tokenService.validateToken(refreshToken, process.env.JWT_SALT_REFRESH) as ClientUserData
+        await this.authService.refreshTokenExits(userData.id, refreshToken)
+
+        const user = await this.userService.getUserBy('userId', userData.id)
+        const clientData = new UserDto(user).toClientDto()
+
+        const {accessToken, refreshToken:refTokenNew} = this.tokenService.generateAccessRefresh(clientData)
+        const expires = new Date();
+        expires.setHours(expires.getHours() + 1);
+
+        await this.authService.updateRefreshToken(user.userId, refTokenNew)
+        res.cookie('refresh', refTokenNew, { httpOnly: true, expires: expires})
+        
+        return {
+            accessToken: accessToken,
+            userData: clientData
+        }
     }
 
     @Get('activate/:link')
